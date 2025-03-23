64383f19504b480f671ead1858c0a92e6c795a8a
zfpxs <2436871821@qq.com>
2025-03-23 15:43:16 +0800

    Update topic.ts

--- aispeak-frontend/src/api/topic.ts
+++ aispeak-frontend/src/api/topic.ts
@@ -24,8 +24,7 @@ export default {
   },
   
   getSessionByLessonId: (data: any) => {
-    const encodedLessonId = encodeURIComponent(data.lesson_id); 
-    return request(`/topics/lesson/${encodedLessonId}/session`, "GET", data, true);
+    return request(`/topics/lesson/${data.lesson_id}/session`, "GET", data, true);
   },
  
   completeTopic: (data: any) => {
